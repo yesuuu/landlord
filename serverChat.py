6e279ef8d6cccb7aa0fd5181ac9caa17438706f3
rxfan <rxfan@wizardquant.com>
2017-12-14 23:32:36 +0800

    ...

--- serverChat.py
+++ serverChat.py
@@ -5,8 +5,9 @@ from asynchat import async_chat
 
 class ChatSession(async_chat):
 
-    def __init__(self, sock):
+    def __init__(self, server, sock):
         async_chat.__init__(self, sock)
+        self.server = server
         self.set_terminator('\r\n')
         self.data = []
 
@@ -17,6 +18,7 @@ class ChatSession(async_chat):
         line = 'b'.join(self.data)
         self.data = []
         print line
+        self.server.broadcast(line)
 
 
 class ChatServer(dispatcher):
@@ -33,10 +35,11 @@ class ChatServer(dispatcher):
     def handle_accept(self):
         conn, addr = self.accept()
         print 'connect from', addr[0]
-        self.sessions.append(ChatSession(conn))
+        self.sessions.append(ChatSession(self, conn))
 
-    def broadcast(self):
-        pass
+    def broadcast(self, info):
+        for ss in self.sessions:
+            ss.push(info)
 
 
 if __name__ == '__main__':
